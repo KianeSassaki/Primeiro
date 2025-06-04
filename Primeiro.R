9ff9546e34b7a3b507f827f2187e395c2d4a8d7f
KianeSassaki <kianesassaki@id.uff.br>
2025-06-04 10:09:00 -0300

    Primeiro script

---
+++ Primeiro.R
@@ -0,0 +1,10 @@
+#Ativando pacote
+library(usethis)
+
+#Registrando dados de usuário
+use_git_config(user.name ="KianeSassaki",user.email ="kianesassaki@id.uff.br" )
+
+#Configurando token
+gitcreds::gitcreds_set()
+
+teste
